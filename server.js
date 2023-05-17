df5b64879b52267d49aa42fc26445052c3692c9b
MihirKotecha <mihir.k1@ahduni.edu.in>
2023-05-17 18:59:42 +0530

    Submission Commit

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const mysql = require('mysql2/promise');
+const ejs = require('ejs');
+const moment = require('moment');
 
 const app = express();
+app.set('view engine', 'ejs');
+
 const port = process.env.PORT || 3000;
 
 const pool = mysql.createPool({
@@ -22,18 +26,58 @@ app.get('/', (req, res) => {
 });
 
 app.post('/login', async (req, res) => {
-    console.log('POST request to /login received');
+  console.log('POST request to /login received');
   const { username, password } = req.body;
   console.log(`username: ${username}, password: ${password}`);
   const conn = await pool.getConnection();
 
   try {
-    const [rows] = await conn.query('SELECT * FROM user WHERE username = ? AND password = ?', [username, password]);
+    const [rows] = await conn.query('SELECT * FROM User WHERE Username = ? AND Password = ?', [username, password]);
     if (rows.length > 0) {
-      res.send(rows);
+      res.sendFile(__dirname + '/ticket-booking.html');
     } else {
       res.send('Invalid username or password');
     }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  } finally {
+    conn.release();
+  }
+});
+
+// Code to search flights based on user input
+app.post('/search-flights', async (req, res) => {
+  const { origin, destination, numPassengers, departureDate } = req.body;
+  const conn = await pool.getConnection();
+
+  try {
+    const [rows] = await conn.query('SELECT * FROM Flight WHERE DepartureAirportCode = ? AND ArrivalAirportCode = ? AND SeatsAvailable >= ? AND DATE(DepartureTime) = ?', [origin, destination, numPassengers, departureDate]);
+    res.render('available_flights', { flights: rows, numPassengers, departureDate });
+    console.log(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  } finally {
+    conn.release();
+  }
+});
+
+// Route for sign-up button
+app.get('/signup', (req, res) => {
+  res.sendFile(__dirname + '/signup.html');
+});
+
+// Route for inserting new user into the User table
+app.post('/signup', async (req, res) => {
+  const { username, password, firstName, lastName } = req.body;
+  console.log(`Received sign-up request with username = ${username}, password = ${password}, firstName = ${firstName}, lastName = ${lastName}`);
+  const conn = await pool.getConnection();
+
+  try {
+    const [result] = await conn.query('INSERT INTO User (UserID, Username, Password, FirstName, LastName) VALUES (UUID(), ?, ?, ?, ?)', [username, password, firstName, lastName]);
+    console.log(`New user with ID ${result.insertId} has been inserted into the User table`);
+    res.redirect('/');
   } catch (err) {
     console.error(err);
     res.status(500).send('Server error');
